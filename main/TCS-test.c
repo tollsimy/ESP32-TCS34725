3c0b002657ccb5f3f8cce9a98299e1c3fd286dbf
tollsimy <simonetollardo@gmail.com>
2022-03-29 16:39:33 +0200

    added infinite loop in main-test

--- main/TCS-test.c
+++ main/TCS-test.c
@@ -13,21 +13,23 @@ uint8_t I2C_PORT=0;
 static const char *TAG = "Demo-TCS34725";
 
 void TCS_task(){
-    float red, green, blue;
-    uint16_t temp, lux;
-    ESP32_TCS34725 TCS={0};
-    if(TCS_init(&TCS)){
-        ESP_LOGI(TAG, "Init complete!");
+    for(;;){
+        float red, green, blue;
+        uint16_t temp, lux;
+        ESP32_TCS34725 TCS={0};
+        if(TCS_init(&TCS)){
+            ESP_LOGI(TAG, "Init complete!");
+        }
+        TCS_enable(&TCS);
+        setGain(&TCS,TCS34725_GAIN_1X);
+        setIntegrationTime(&TCS,TCS34725_INTEGRATIONTIME_50MS);
+        getRGB(&TCS,&red,&green,&blue);
+        temp=calculateColorTemperature(red,green,blue);
+        lux=calculateLux(red,green,blue);
+        ESP_LOGI(TAG, "RED: %f, GREEN: %f, BLUE: %f", red, green, blue);
+        ESP_LOGI(TAG, "Color Temperature: %d, LUX: %d", temp, lux);
+        vTaskDelay(3000/portTICK_PERIOD_MS);
     }
-    TCS_enable(&TCS);
-    setGain(&TCS,TCS34725_GAIN_1X);
-    setIntegrationTime(&TCS,TCS34725_INTEGRATIONTIME_50MS);
-    getRGB(&TCS,&red,&green,&blue);
-    temp=calculateColorTemperature(red,green,blue);
-    lux=calculateLux(red,green,blue);
-    ESP_LOGI(TAG, "RED: %f, GREEN: %f, BLUE: %f", red, green, blue);
-    ESP_LOGI(TAG, "Color Temperature: %d, LUX: %d", temp, lux);
-    vTaskDelay(3000/portTICK_PERIOD_MS);
 }
 
 void app_main(void)
